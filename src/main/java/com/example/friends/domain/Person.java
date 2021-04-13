b78e65816750d0ffde69141e041cc8bc80bf2ec4
cheogodzip <lkver@naver.com>
2021-04-13 10:25:04 +0900

    jpa-03강 lombok(2)-1

--- src/main/java/com/example/friends/domain/Person.java
+++ src/main/java/com/example/friends/domain/Person.java
@@ -1,8 +1,6 @@
 package com.example.friends.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,13 +11,18 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Person {
     @Id
     @GeneratedValue()
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
     private String hobby;
@@ -34,4 +37,22 @@ public class Person {
 
     @ToString.Exclude
     private String phoneNumber;
+
+    @Override
+    public boolean equals(Object object){
+        if (object == null){
+            return false;
+        }
+        Person person = (Person)object;
+
+        if(!person.getName().equals(this.getName())){
+            return false;
+        }
+
+        if (person.getAge() != (this.getAge())) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- src/test/java/com/example/friends/repository/PersonRepositoryTest.java
+++ src/test/java/com/example/friends/repository/PersonRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -32,4 +33,13 @@ class PersonRepositoryTest {
         Assertions.assertThat(people.get(0).getBloodType()).isEqualTo("A");
     }
 
+    @Test
+    void hashCodeAndEquals(){
+        Person person1 = new Person("martin", 10);
+        Person person2 = new Person("martin", 10);
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+    }
 }
