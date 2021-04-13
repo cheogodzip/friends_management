19771cee18dbf2c146a9187948a96511c8bef92f
cheogodzip <lkver@naver.com>
2021-04-13 10:51:51 +0900

    jpa-04강 lombok(2)-2

--- src/main/java/com/example/friends/domain/Person.java
+++ src/main/java/com/example/friends/domain/Person.java
@@ -8,12 +8,10 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class Person {
     @Id
     @GeneratedValue()
@@ -27,6 +25,7 @@ public class Person {
 
     private String hobby;
 
+    @NonNull
     private String bloodType;
 
     private String address;
@@ -37,22 +36,4 @@ public class Person {
 
     @ToString.Exclude
     private String phoneNumber;
-
-    @Override
-    public boolean equals(Object object){
-        if (object == null){
-            return false;
-        }
-        Person person = (Person)object;
-
-        if(!person.getName().equals(this.getName())){
-            return false;
-        }
-
-        if (person.getAge() != (this.getAge())) {
-            return false;
-        }
-
-        return true;
-    }
 }
