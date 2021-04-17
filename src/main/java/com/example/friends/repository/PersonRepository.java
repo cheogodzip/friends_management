a18a1e057cbc5fbb98ebddde01e13b7cb064c106
cheogodzip <lkver@naver.com>
2021-04-17 16:48:42 +0900

    chapter04-05강까지

--- src/main/java/com/example/friends/repository/PersonRepository.java
+++ src/main/java/com/example/friends/repository/PersonRepository.java
@@ -2,6 +2,18 @@ package com.example.friends.repository;
 
 import com.example.friends.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long>{
+    List<Person> findByName(String name);
+
+    List<Person> findByBlockIsNull();
+
+    List<Person> findByBloodType(String bloodType);
+
+    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday")
+    List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday);
 }
