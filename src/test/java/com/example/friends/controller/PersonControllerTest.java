cf18cdd31b5707ad7f474777e3bdf206999ecdc1
cheogodzip <lkver@naver.com>
2021-04-18 23:30:33 +0900

    테스트 코드 수정

--- src/test/java/com/example/friends/controller/PersonControllerTest.java
+++ src/test/java/com/example/friends/controller/PersonControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -84,5 +83,6 @@ class PersonControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
         log.info("people deleted : {}", personRepository.findPeopleDeleted());
+//        System.out.println(personRepository.findPeopleDeleted());
     }
 }
