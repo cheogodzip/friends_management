ecfd3cec455b7e5e951ce2565005502d9c1c9dcb
cheogodzip <lkver@naver.com>
2021-04-23 20:28:40 +0900

    parameter 유해성 로직 추가

--- src/main/java/com/example/friends/controller/dto/PersonDto.java
+++ src/main/java/com/example/friends/controller/dto/PersonDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
@@ -11,10 +12,16 @@ import java.time.LocalDate;
 @AllArgsConstructor(staticName = "of")
 public class PersonDto {
 
+    @NotBlank(message = "이름은 필수값입니다")
     private String name;
+
     private String hobby;
+
     private String address;
+
     private LocalDate birthday;
+
     private String job;
+
     private String phoneNumber;
 }
